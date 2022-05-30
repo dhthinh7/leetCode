0c391939fe23872752ad4609b2aa7f502e95b2c2
dhthinh7 <84767360+dhthinh7@users.noreply.github.com>
2022-05-30 16:55:16 +0700

    Update RemoveDupFromArray.js

--- RemoveDupFromArray.js
+++ RemoveDupFromArray.js
@@ -1,18 +1,19 @@
-// Remove Duplicates from Sorted Array
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
 
+// Remove Duplicates from Sorted Array
 // Input: nums = [0,0,1,1,1,2,2,3,3,4]
 // Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
 // Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
 // It does not matter what you leave beyond the returned k (hence they are underscores).
-
-nums = [0,0,1,1,1,2,2,3,3,4,4,8]
+nums = [0,0,1,1,1,2,2,3,3,4]
 var removeDuplicates = function(nums) {
-    var tempNums = [];
-    var tempNull = [];
-    nums.map((item, index) => {
-        if (!tempNums.includes(item)) tempNums.push(item);
-        else tempNull.push("-");
+    var count = 0;
+    nums.map((item, i) => {
+        if (nums[i+1] != nums[i]){
+            nums[count++] = nums[i];
+        };
     })
-    console.log(tempNums.length, [...tempNums, ...tempNull], [...tempNums, ...tempNull].length);
+  return(count);
 };
-removeDuplicates(nums);
+console.log(removeDuplicates(nums));
+console.log(nums);
